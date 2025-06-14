6825a1ca0ccdd440367ef45572637a75da0a918d
Gekif <dzulfikar.maulana@gmail.com>
2025-06-14 21:57:37 +0700

    Membuat multiple isi

--- todo_project/todo/views.py
+++ todo_project/todo/views.py
@@ -1,7 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import TodoItem
 from .serializers import TodoItemSerializer
 
 class TodoItemViewSet(viewsets.ModelViewSet):
     queryset = TodoItem.objects.all()
     serializer_class = TodoItemSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Cek tipe data: list atau dict
+        is_many = isinstance(request.data, list)
+
+        serializer = self.get_serializer(data=request.data, many=is_many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
